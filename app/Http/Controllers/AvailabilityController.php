12a37d5ca3d5d55d06cfdedaa977681619a1e207
deviser <deviserops+ubuntu@gmail.com>
2024-08-09 11:38:55 +0530

    updates

--- app/Http/Controllers/AvailabilityController.php
+++ app/Http/Controllers/AvailabilityController.php
@@ -3,13 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Availability;
-use App\Models\User;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
-use Illuminate\Validation\Rules;
 use Inertia\Response;
 
 class AvailabilityController extends Controller {
@@ -20,26 +15,6 @@ class AvailabilityController extends Controller {
         return Inertia::render('Availability/Index', [
             'availability' => $availability,
         ]);
-    }
-
-    public function update(Request $request): RedirectResponse {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'middle_name' => 'string|max:255|nullable',
-            'last_name' => 'string|max:255|nullable',
-            'email' => 'required|string|email|max:255|unique:' . User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
-
-        User::create([
-            'name' => $request->name,
-            'middle_name' => $request->middle_name,
-            'last_name' => $request->last_name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        return redirect()->route('student');
     }
 
 }
