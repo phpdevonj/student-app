1f8175edbcbde77fbcb9f6eb2e902d6956171f95
deviser <deviserops+ubuntu@gmail.com>
2024-08-12 19:33:26 +0530

    updates

--- resources/js/Pages/Report/index.vue
+++ resources/js/Pages/Report/index.vue
@@ -18,19 +18,20 @@ defineProps({
     <AuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Report Card</h2>
-            <div class="py-12">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                        <UploadReportForm
-                            class="max-w-full"
-                        />
-                        <ReportList
-                            :reports="reports"
-                            class="max-w-full"
-                        />
-                    </div>
+        </template>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <UploadReportForm
+                        class="max-w-full"
+                    />
+                    <ReportList
+                        :reports="reports"
+                        class="max-w-full"
+                    />
                 </div>
             </div>
-        </template>
+        </div>
+
     </AuthenticatedLayout>
 </template>
