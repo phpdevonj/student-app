ea467d2760f5be1a1bf8e8cf3124b6b74c0bedc7
phpdev <phpdevelopment@ongraph.com>
2024-08-12 23:28:51 +0530

    Hide uploads option for student user

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -41,7 +41,7 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink v-if="$page.props.auth.user.type == 1" :href="route('schedules')" :active="route().current('schedules')">
                                     Schedules
                                 </NavLink>
-                                <NavLink :href="route('report.index')" :active="route().current('report')">
+                                <NavLink v-if="$page.props.auth.user.type == 1" :href="route('report.index')" :active="route().current('report')">
                                     Uploads
                                 </NavLink>
                             </div>
